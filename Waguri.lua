98c7978892124bc5d8d8349e30c312b8c289d334
Waguriiii <vjramon875@gmail.com>
2026-07-21 19:36:58 +0800

    Update Waguri.lua

--- Waguri.lua
+++ Waguri.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/JmJnXdKaDp/0", true))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/jntLb8Z2sF/0", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/EUVMnkePZG/0", true))()
